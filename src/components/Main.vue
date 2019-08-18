073861c0acd264d47078292cbf14a9d20e925fa7
sunyiyan <1481249319@qq.com>
2019-08-18 11:06:54 +0800

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-container>
+        <el-header id = "test">{{msg}}</el-header>
+    </el-container>
+
+</template>
+
+<script>
+new Vue({
+    el:'#test',
+    data(){
+        msg:'绿林花卉'
+    }
+
+}) 
+</script>
