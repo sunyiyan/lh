f1a8046562162add17a0ac5a548d3b8cd9ba6763
sunyiyan <1481249319@qq.com>
2019-09-07 21:35:13 +0800

    幻灯片和导航菜单添加完成

--- src/components/Enter.vue
+++ src/components/Enter.vue
@@ -2,20 +2,110 @@
 <template>
   <div id="enter">
     <el-container>
-
       <el-header>Header</el-header>
-      <el-header>Menu</el-header>
-      <el-header height = "200px">
+      <el-header>
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">花灌木类</el-menu-item>
+            <el-menu-item index="2-2">行道树类</el-menu-item>
+            <el-menu-item index="2-3">藤本植物类</el-menu-item>
+            <el-menu-item index="2-4">宿根草本类</el-menu-item>
+            <el-menu-item index="2-5">松柏长青类</el-menu-item>
+            <el-menu-item index="2-6">水生植物类</el-menu-item>
+            <el-menu-item index="2-7">绿化小苗</el-menu-item>
+            <el-menu-item index="2-8">花卉种子</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">行情资讯</template>
+            <el-menu-item index="3-1">花灌木类</el-menu-item>
+            <el-menu-item index="3-2">行道树类</el-menu-item>
+            <el-menu-item index="3-3">藤本植物类</el-menu-item>
+            <el-menu-item index="3-4">宿根草本类</el-menu-item>
+            <el-menu-item index="3-5">松柏长青类</el-menu-item>
+            <el-menu-item index="3-6">水生植物类</el-menu-item>
+            <el-menu-item index="3-7">绿化小苗</el-menu-item>
+            <el-menu-item index="3-8">花卉种子</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">新闻频道</template>
+            <el-menu-item index="4-1">花灌木类</el-menu-item>
+            <el-menu-item index="4-2">行道树类</el-menu-item>
+            <el-menu-item index="4-3">藤本植物类</el-menu-item>
+            <el-menu-item index="4-4">宿根草本类</el-menu-item>
+            <el-menu-item index="4-5">松柏长青类</el-menu-item>
+            <el-menu-item index="4-6">水生植物类</el-menu-item>
+            <el-menu-item index="4-7">绿化小苗</el-menu-item>
+            <el-menu-item index="4-8">花卉种子</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">供应信息</template>
+            <el-menu-item index="5-1">花灌木类</el-menu-item>
+            <el-menu-item index="5-2">行道树类</el-menu-item>
+            <el-menu-item index="5-3">藤本植物类</el-menu-item>
+            <el-menu-item index="5-4">宿根草本类</el-menu-item>
+            <el-menu-item index="5-5">松柏长青类</el-menu-item>
+            <el-menu-item index="5-6">水生植物类</el-menu-item>
+            <el-menu-item index="5-7">绿化小苗</el-menu-item>
+            <el-menu-item index="5-8">花卉种子</el-menu-item>
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title">苗木专场</template>
+            <el-menu-item index="6-1">花灌木类</el-menu-item>
+            <el-menu-item index="6-2">行道树类</el-menu-item>
+            <el-menu-item index="6-3">藤本植物类</el-menu-item>
+            <el-menu-item index="6-4">宿根草本类</el-menu-item>
+            <el-menu-item index="6-5">松柏长青类</el-menu-item>
+            <el-menu-item index="6-6">水生植物类</el-menu-item>
+            <el-menu-item index="6-7">绿化小苗</el-menu-item>
+            <el-menu-item index="6-8">花卉种子</el-menu-item>
+          </el-submenu>
+          <el-submenu index="7">
+            <template slot="title">公司简介</template>
+            <el-menu-item index="7-1">花灌木类</el-menu-item>
+            <el-menu-item index="7-2">行道树类</el-menu-item>
+            <el-menu-item index="7-3">藤本植物类</el-menu-item>
+            <el-menu-item index="7-4">宿根草本类</el-menu-item>
+            <el-menu-item index="7-5">松柏长青类</el-menu-item>
+            <el-menu-item index="7-6">水生植物类</el-menu-item>
+            <el-menu-item index="7-7">绿化小苗</el-menu-item>
+            <el-menu-item index="7-8">花卉种子</el-menu-item>
+          </el-submenu>
+          <el-submenu index="8">
+            <template slot="title">联系我们</template>
+            <el-menu-item index="8-1">花灌木类</el-menu-item>
+            <el-menu-item index="8-2">行道树类</el-menu-item>
+            <el-menu-item index="8-3">藤本植物类</el-menu-item>
+            <el-menu-item index="8-4">宿根草本类</el-menu-item>
+            <el-menu-item index="8-5">松柏长青类</el-menu-item>
+            <el-menu-item index="8-6">水生植物类</el-menu-item>
+            <el-menu-item index="8-7">绿化小苗</el-menu-item>
+            <el-menu-item index="8-8">花卉种子</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-header>
+      <el-header height="200px">
         <el-carousel
-          :interval="4000"
-          type="card"
+          indicator-position="outside"
           height="200px"
         >
           <el-carousel-item
-            v-for="item in 6"
+            v-for="item in urls"
             :key="item"
           >
-            <h3 class="medium">{{ item }}</h3>
+            <el-image
+              :src="item"
+              class="el-carousel__item h3"
+            ></el-image>
           </el-carousel-item>
         </el-carousel>
       </el-header>
@@ -35,7 +125,18 @@
 export default {
   name: "Enter",
   data() {
-    return {};
+    return {
+      urls: [
+        require('@/assets/banner_a.jpg'),
+        require('@/assets/banner_8.jpg'),
+        require('@/assets/banner_4.jpg')
+      ]
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
 };
 </script>
